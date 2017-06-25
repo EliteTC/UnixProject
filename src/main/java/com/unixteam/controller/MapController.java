41af8d760493e05a741dfbf8d0c329cad6466f62
Вадим Томащук < >
2017-06-25 22:26:41 +0300

    DB for deploying

---
+++ src/main/java/com/unixteam/controller/MapController.java
@@ -0,0 +1,11 @@
+package com.unixteam.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Вадим on 25.06.2017.
+ */
+@RestController
+public class MapController {
+
+}
